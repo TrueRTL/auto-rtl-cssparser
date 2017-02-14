22240e96b84ac8a6881b782384bf04c1fd2e537b
Seyed Ali Roshan <seyedalir@yahoo.com>
2017-02-15 02:04:12 +0330

    add new tests for fileTools class and methods

--- src/test/java/tools/FileToolsTest.java
+++ src/test/java/tools/FileToolsTest.java
@@ -1,19 +1,35 @@
 package tools;
 
+import org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by seyed on 2/8/17.
  */
 public class FileToolsTest {
-    @org.junit.Test
-    public void fileToStream() throws Exception {
+    private InputStream streamFile;
 
+    @Test
+    public void fileToStream() throws Exception {
+        assertThat(
+                new tools.FileTools().fileToStream("style.css"),
+                org.hamcrest.CoreMatchers.instanceOf(InputStream.class));
     }
 
-    @org.junit.Test
-    public void streamToString() throws Exception {
+    @Before
+    public void beforeStreamToString() throws  Exception{
+        this.streamFile = new tools.FileTools().fileToStream("style.css");
+    }
 
+    @Test
+    public void streamToString() throws Exception {
+        assertThat(new tools.FileTools().streamToString(this.streamFile),
+                org.hamcrest.CoreMatchers.instanceOf(String.class));
     }
 
 }
